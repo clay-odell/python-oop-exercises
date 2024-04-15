074dd425c29140211db16e7d36cc13f8f2f1aee3
Clay O'Dell <clay.odell.music@gmail.com>
2024-04-15 12:53:40 -0500

    filtered out blank lines and lines that start with #

--- wordfinder.py
+++ wordfinder.py
@@ -9,24 +9,10 @@ class WordFinder:
     def read_words(self, file_path):
         try:
             with open(file_path, 'r') as file:
-                return [line.strip() for line in file]
+                return [line.strip() for line in file if line.strip() and not line.startswith('#')]
         except FileNotFoundError:
             print("The file was not found.")
             return []
         
     def random(self):
         return random.choice(self.words) if self.words else None
->>> wf = WordFinder("/Users/student/words.txt")
-3 words read
-
->>> wf.random()
-'cat'
-
->>> wf.random()
-'cat'
-
->>> wf.random()
-'porcupine'
-
->>> wf.random()
-'dog'
