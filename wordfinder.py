e05467a6666f876fb49fefced342e7013d795227
Clay O'Dell <clay.odell.music@gmail.com>
2024-04-15 12:52:24 -0500

    wordfinder completed

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,32 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
-    ...
+    def __init__(self, file_path):
+        self.words = self.read_words(file_path)
+        print(f"{len(self.words)} words read")
+    
+    def read_words(self, file_path):
+        try:
+            with open(file_path, 'r') as file:
+                return [line.strip() for line in file]
+        except FileNotFoundError:
+            print("The file was not found.")
+            return []
+        
+    def random(self):
+        return random.choice(self.words) if self.words else None
+>>> wf = WordFinder("/Users/student/words.txt")
+3 words read
+
+>>> wf.random()
+'cat'
+
+>>> wf.random()
+'cat'
+
+>>> wf.random()
+'porcupine'
+
+>>> wf.random()
+'dog'
